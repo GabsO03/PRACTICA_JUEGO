49e56aa321f9498d176a18636854c9f87c344373
Marcos <marcos.contreras.1306@fernando3martos.com>
2024-01-26 10:48:12 +0100

    Carpeta creada

--- src/Clases/Complemento.java
+++ src/Clases/Complemento.java
@@ -1,4 +1,11 @@
 package Clases;
-
+import java.util.Scanner;
 public class Complemento {
+    private String Complemento;
+    public String getComplemento() {
+        return Complemento;
+    }
+    public void setComplemento(String complemento) {
+        Complemento = complemento;
+    }
 }
